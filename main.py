26794efa0d2913ae3893caadea22af8c9f55fc2b
Teeny <largo@pop-os.localdomain>
2021-10-28 21:48:10 +0300

    buttons work, but may be problem with triggering

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def handle_start_help(msg):
 
 @bot.message_handler(commands=['me'])
 def handle_me(msg):
-    text = msg.text.split("/me")[1]
+    text = msg.text.split("/me")[1].lstrip()
     print(text)
     me = "{} {}".format(msg.from_user.first_name, text)
     bot.send_message(msg.chat.id, me)
@@ -31,38 +31,49 @@ def handle_me(msg):
 
 @bot.message_handler(content_types=["text"])
 def handle_commands(msg):
-    # print('dbg1: some event handled')
-    if msg.text.lower() == '!ретритнись':
-        print('dbg2: new poll')
+    print('dbg1: some event handled')
+    # TODO проверить, что это реплай, если нет, ответить, что нужен реплай
+    # TODO искать подстроку, а не полное соотвветсвие
+    mtext = msg.text.lower()
+    if mtext == '!ретритнись' or mtext == '!ретрит' or mtext == '!р':
+        print('dbg2: need poll')
         bot.send_message(msg.chat.id, 'Уже читаю мантры')
-        time.sleep(0.5)  # for humanityzm
+
         if msg.reply_to_message.from_user.first_name is None:
-            user = msg.reply_to_message.from_user.username
+            from_user = msg.reply_to_message.from_user.username
         else:
-            user = msg.reply_to_message.from_user.first_name
-        create_poll(msg.chat.id, user, msg.message_id)
+            from_user = msg.reply_to_message.from_user.first_name
+
+        # bot.register_next_step_handler(msg, create_poll, from_user)
+        # time.sleep(0.5)  # for humanityzm
+        create_poll(msg, from_user)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def check_all_messages(msg):
-    print('dbg2: key pressed')
+    print('dbg1: key pressed')
     print(msg)
-    handle_btn_press(msg.message.chat.id, msg.data, msg.from_user.id, msg.message.message_id)
+    handle_btn_press(msg.message.chat.id, msg.data, msg.from_user.id, msg.message.text)
 
 
-def handle_btn_press(chat_id, btn_callback, from_user, msg_id):
+def handle_btn_press(chat_id, btn_callback, from_user, text):
+    print("dbg2: try to register keypress: {}".format(btn_callback))
     variant = session.query(Variants).\
         filter(Variants.variant_callback == btn_callback).first()
     poll_id = variant.poll_id
-
+    print("dbg2.1: poll ID: {}".format(poll_id))
     voted = session.query(Voters).\
         filter(and_(Voters.poll_id == poll_id,
                     Voters.user_id == from_user)).first()
+
     if voted is None:
-        vote_in_poll(poll_id, from_user, variant)
+        print("dbg2.1: User not voted in this poll, try register and update kbd")
+        cb_y, cb_n, poll = vote_in_poll(poll_id, from_user, variant)
+        send_kbd(chat_id, text, cb_y, cb_n, poll_id)
 
 
 def vote_in_poll(poll_id, from_user, variant):
+    print("dbg3: register vote")
     poll = session.query(Polls).filter(Polls.id == poll_id).first()
     if variant.yes_no == 'yes':
         voted_now = Voters(poll_id=poll_id,
@@ -87,31 +98,55 @@ def vote_in_poll(poll_id, from_user, variant):
     session.add(voted_now)
     session.commit()
 
-    send_kbd(chat_id, msg_id, cb_y, cb_n, poll)
+    return cb_y, cb_n, poll
 
 
-def create_poll(chat_id, user, poll_id):
+def create_poll(msg, user):
+    print("dbg2.5: try create poll")
     caption = '{} отправляется на випассану?'.format(user)
-    cb_yes, cb_no = create_poll_in_db(chat_id, poll_id)
-    send_kbd(chat_id, caption, cb_yes, cb_no)
+    poll_id = str(msg.chat.id) + str(msg.message_id)
+    cb_yes, cb_no = create_poll_in_db(msg.chat.id, poll_id)
+    send_kbd(msg.chat.id, caption, cb_yes, cb_no, poll_id)
 
 
-def send_kbd(chat_id, caption, cb_yes, cb_no, poll=None):
-    mrkp = telebot.types.InlineKeyboardMarkup(row_width=2)
-    btn_yes = telebot.types.InlineKeyboardButton(text='Да', callback_data=cb_yes)
-    mrkp.add(btn_yes)
-    btn_no = telebot.types.InlineKeyboardButton(text='Нет', callback_data=cb_no)
-    mrkp.add(btn_no)
-    if isinstance(caption, str):
-        bot.send_message(chat_id, caption, reply_markup=mrkp)
+def send_kbd(chat_id, caption, cb_yes, cb_no, pid):
+    print("dbg3: try to send kbd | poll PID: {}".format(pid))
+    poll = session.query(Polls).filter(Polls.pid == pid).first()
+    if poll is None:
+        poll = session.query(Polls).filter(Polls.id == pid).first()
+    mrkp = telebot.types.InlineKeyboardMarkup(row_width=1)
+    if poll is None:
+        print("dbg3.1: no poll, setting buttons to zero")
+        yes_count, no_count = 0, 0
     else:
+        print("dbg3.1: have poll")
+        yes_count, no_count = poll.yes_count, poll.no_count
+
+    btn_yes = telebot.types.InlineKeyboardButton(text='Да ({})'.format(yes_count), callback_data=cb_yes)
+    mrkp.add(btn_yes)
+    btn_no = telebot.types.InlineKeyboardButton(text='Нет ({})'.format(no_count), callback_data=cb_no)
+    mrkp.add(btn_no)
+    # If new than send, else edit
+    if (yes_count == 0 and no_count == 0) and (cb_yes or cb_no):
+        new_poll = bot.send_message(chat_id=chat_id,
+                                    reply_markup=mrkp,
+                                    text=caption)
+        poll.pid = new_poll.id
+        session.commit()
+        print("dbg3.2 new poll: {}".format(new_poll))
+    else:
+        pid = str(poll.pid)
+        print('dbg3.2 pid: {} {}, {}'.format(pid, type(pid), chat_id))
+        # poll_id = pid.split(str(chat_id))[1]
+        poll_id = pid.split(str(chat_id))
+        print("dbg3.3: editting msg: {}".format(poll_id))
         bot.edit_message_text(chat_id=chat_id,
-                              message_id=caption,
+                              message_id=poll_id,
                               reply_markup=mrkp,
-                              text="{}\nЗа - {}\nПротив - {}".format(poll.text, poll.yes_count, poll.no_count))
+                              text=caption)
 
 
-def create_poll_in_db(caption, chat_id, polls_id=None):
+def create_poll_in_db(caption, poll_id):
     try:
         last_id = session.query(Polls.id).order_by(Polls.id.desc()).first()[0]
     except Exception as e:
@@ -122,6 +157,7 @@ def create_poll_in_db(caption, chat_id, polls_id=None):
     callback_no = callback_generator()
 
     poll_row = Polls(id=int(last_id)+1,
+                     pid=poll_id,
                      text=caption,
                      yes_count=0,
                      no_count=0)
